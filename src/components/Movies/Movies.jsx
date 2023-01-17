157dc980ba179e7727e3b244ef25590994c7ac30
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-01-17 23:36:52 +0700

    #1 create the Preloader component

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,12 +2,14 @@ import './Movies.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
 
 function Movies() {
   return (
     <div>
       <Header />
       <SearchForm />
+      <Preloader />
       <Footer />
     </div>
   );
